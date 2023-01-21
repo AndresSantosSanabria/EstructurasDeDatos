32a12a6eb555000c9f5589988871ca503abbdefb
Andres <andressantos1544@gmail.com>
2023-01-21 12:17:35 -0500

    Ejercicio 1

--- Ejercicio_1/src/App.java
+++ Ejercicio_1/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite un numero");
+        double numero = teclado.nextDouble();
+
+        if (numero<0) {
+            System.out.println("El valor absoluto de \""+numero+"\" es: \""+numero*-1+"\"");
+        }else {
+            System.out.println("El valor absoluto de \""+numero+"\" numero es: \""+numero+"\"");
+        }
+
     }
 }
