0021de34bcfb8342f93d5635b8789d4aa6684834
Andres <andressantos1544@gmail.com>
2023-01-21 14:48:44 -0500

    Raiz cuadrada

---
+++ Ejercicio_5/src/RaizCuadrada.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RaizCuadrada {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite un numero");
+        double numero = teclado.nextInt();
+        if (numero>=0) {
+            
+            System.out.println("La raiz cuadrada del numero \""+ numero+"\" es:\""+(Math.sqrt(numero))+"\"");
+        }else{
+            System.out.println("El único número que tiene una sola raíz cuadrada es el cero."+ 
+                            "Por el mismo motivo, no existen raíces cuadradas de número negativos.");
+        }
+    }
+}
